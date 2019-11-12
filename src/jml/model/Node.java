c54ca9a68e5f2fdf3d4b01c295ac289d7bd6404e
Keshav Iyengar <dearkri03@gmail.com>
2019-11-12 18:23:29 -0500

    Changed node class and added connection class

--- src/jml/model/Node.java
+++ src/jml/model/Node.java
@@ -1,18 +1,43 @@
 package jml.model;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-public class Node {
-	private ArrayList<Double> weights;
+public class Node {	
 	
-	public Node(int numInputs) {
-		Random rand = new Random();
-		
-		weights = new ArrayList<Double>(numInputs + 1);
-		
-		for (int i = 0; i < numInputs + 1; i++) {
-			weights.set(i, rand.nextDouble());
+	private double total;
+	
+	private Connection[] connections;
+	
+	public Node() {
+		total = 0;
+	}
+	public Node(int numConnect) {
+		connections = new Connection[numConnect];
+		for(int i = 0; i < numConnect; i++) {
+			connections[i] = new Connection();
 		}
+	}
+	public void setSum(double[] input) {
+		total = 0;
+		for(double part:input) {
+			total += part;
+		}
+	}
+	public void setSum(double input) {
+		total = input;
+	}
+	public double sigmoid() {
+		return (1/(1 + Math.pow(Math.E, total)));
+	}
+	public void setInpPerceptron() {
+		for(int i = 0; i < connections.length; i++) {
+			connections[i].setWeight(1);
+		}
+	}
+	
+	public String toString() {
+		String returnString = "";
+		for(int i = 0; i < connections.length; i++) {
+			returnString += "\nConnection " + Integer.toString(i+1) + ":   " + connections[i].toString();
+		}
+		return returnString;
 	}
 }
