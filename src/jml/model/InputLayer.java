211a9fd24ccddc63c3073183320739fb9701b9a0
Aarya Patel <aarya.patel@gmail.com>
2019-11-13 13:18:37 +0000

    Revert "reseting input layer, may be an accident but my code didnt push and  it"
    This reverts commit b01a52c7debb6f0e10f49abad61d8828db810ecb

--- src/jml/model/InputLayer.java
+++ src/jml/model/InputLayer.java
@@ -1,12 +1,18 @@
 package jml.model;
 
-import java.util.ArrayList;
-
-public class InputLayer {
-	private double[] inputs;
+public class InputLayer extends Layer{
 	
-	public InputLayer(int numInputs) {
-		inputs = new double[numInputs];
+	public InputLayer(final int numInputs)  {
+		super();
+		nodes = new Node[numInputs];
+		for (int i = 0; i < numInputs	; i++) {
+			nodes[i] =  new Node(1);
+		}
+	}
+	
+	public double[] calculate(double[] previousLayerOutputs) {
+		return previousLayerOutputs;
+		
 	}
 	public double[] getInputs() {
 		return inputs;
