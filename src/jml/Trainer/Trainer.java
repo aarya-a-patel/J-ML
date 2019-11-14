fe4d1f841528fe3d00272ffff4846c98d4494f44
Keshav Iyengar <dearkri03@gmail.com>
2019-11-14 17:54:38 -0500

    Moved trainer class to new package

---
+++ src/jml/Trainer/Trainer.java
@@ -0,0 +1,10 @@
+package jml.Trainer;
+
+import jml.model.Model;
+
+public class Trainer extends Model {
+
+	public Trainer(final int layerSizes[]){
+		super(layerSizes);
+	}
+}
--- src/jml/model/Training.java
+++
@@ -1,7 +0,0 @@
-package jml.model;
-
-public class Training extends Model {
-	public Training(final int layerSizes[]){
-		super(layerSizes);
-	}
-}
