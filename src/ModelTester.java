5e25d90e6c5daadd0c0e37de32b0a1ce26b66c1b
Aarya Patel <aarya.patel@gmail.com>
2019-11-14 17:35:11 -0500

    Testing

--- src/ModelTester.java
+++ src/ModelTester.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 import jml.model.Model;
 
 public class ModelTester {
 	public static void main(String[] args) {
-		int[] nodes = {2,3,3,1};
+		int[] nodes = { 2, 1 };
 		Model m = new Model(nodes);
-		double[] input = {1,2};
-		m.feedForward(input);
+		double[] input = { 1 , 3 };
+		System.out.println(Arrays.toString(m.feedForward(input)));
+		input[0] = 2;
+		System.out.println(Arrays.toString(m.feedForward(input)));
+		input[0] = 3;
+		System.out.println(Arrays.toString(m.feedForward(input)));
 		System.out.println(m.toString());
 	}
 }
