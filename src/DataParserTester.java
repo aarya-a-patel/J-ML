57ea1bbdcfd83da55b3a3e4f1d750343bb04f178
Aarya Patel <aarya.patel@gmail.com>
2019-11-14 18:20:03 -0500

    Updated Trainer constructor and instance variables

---
+++ src/DataParserTester.java
@@ -0,0 +1,23 @@
+import jml.trainer.DataParser;
+
+public class DataParserTester implements DataParser {
+
+	@Override
+	public int[] getBatches(String pathOfData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getBatchData(int batchNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] parseData(int batchNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/jml/trainer/Trainer.java
+++ src/jml/trainer/Trainer.java
@@ -3,12 +3,19 @@ package jml.trainer;
 import jml.model.Model;
 import jml.matrix.Matrix;
 
-public class Trainer extends Model {
+public class Trainer extends Model implements Runnable {
 
 	private Matrix inputs;
 	private Matrix answers;
 	
+	private DataParser data;
+	
 	public Trainer(Model model, DataParser data){
 		super(model);
+		this.data = data;
+	}
+
+	public void run() {
+		
 	}
 }
