e0cb62fa2a07171e25f00d00b0548a0c72a37dfd
Keshav Iyengar <dearkri03@gmail.com>
2019-11-14 17:57:25 -0500

    Added Matrix class to Trainer class

--- src/jml/Trainer/Trainer.java
+++ src/jml/Trainer/Trainer.java
@@ -1,9 +1,13 @@
 package jml.Trainer;
 
 import jml.model.Model;
+import jml.matrix.Matrix;
 
 public class Trainer extends Model {
 
+	private Matrix inputs;
+	private Matrix answers;
+	
 	public Trainer(final int layerSizes[]){
 		super(layerSizes);
 	}
