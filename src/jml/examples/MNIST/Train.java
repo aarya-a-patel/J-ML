77b6358043f2cc75fd17273283252d9c95598c1c
Aarya Patel <aarya.patel@gmail.com>
2019-11-28 14:57:20 -0500

    Adjust learning rate
    Former-commit-id: dcd15850bf30ca14cd4ae42cac926915745da1b2

--- src/jml/examples/MNIST/Train.java
+++ src/jml/examples/MNIST/Train.java
@@ -13,24 +13,24 @@ import jml.trainer.Trainer;
 public class Train {
 
 	public static void main(String[] args) {
-		int[] layerSizes = { 784, 18, 18, 10 };
+		int[] layerSizes = { 784, 16, 16, 10 };
 
 		Model m = new Model(layerSizes, true);
 		Parser dp = new Parser();
 		Trainer t = new Trainer(m, dp);
-		
+
 		dp.openFile("src\\jml\\examples\\MNIST\\mnist_train.csv");
-		
+
 		t.start();
 	}
 
 	private static class Parser implements DataParser {
-		
+
 		ArrayList<Matrix> inputs = new ArrayList<Matrix>();
 		ArrayList<Matrix> actuals = new ArrayList<Matrix>();
-		
+
 		int index = 0;
-		
+
 		public int[] getBatches(String pathOfData) {
 			return null;
 		}
@@ -55,30 +55,30 @@ public class Train {
 			double[][] input = new double[1][784];
 			double[][] actual = new double[1][10];
 			String[] data;
-			
+
 			try {
 				Scanner file = new Scanner(new File(fileName));
-				
+
 				for (int i = 0; file.hasNextLine(); i++) {
 					data = file.nextLine().split(",");
-					
+
 					actual = new double[1][10];
 					input = new double[1][784];
-					
+
 					for (int j = 0; j < 10; j++) {
 						actual[0][j] = 0;
 					}
-					
+
 					actual[0][Integer.parseInt(data[0])] = 1;
-					
+
 					for (int j = 0; j < 784; j++) {
 						input[0][j] = Integer.parseInt(data[j + 1]);
 					}
-					
+
 					inputs.add(new Matrix(input));
 					actuals.add(new Matrix(actual));
 				}
-				
+
 				file.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
